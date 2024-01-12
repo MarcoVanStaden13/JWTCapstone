2910441c6ecdab1d864bb2a1cf519aa5b766c3b3
Jakkalsxl <marco@thereef.co.za>
2024-01-12 18:56:19 +0200

    Refactor capitalizeFirstLetter function, removing underscores and capitalizing each word

--- frontend/src/Components/DataDisplay.js
+++ frontend/src/Components/DataDisplay.js
@@ -6,45 +6,53 @@ function DataDisplay(props) {
     const [fetchedData, setFetchedData] = useState([]);
 
     useEffect(() => {
-      setFetchedData(JSON.parse(props.data));
+        setFetchedData(JSON.parse(props.data));
     }, [props.data]);
-  
+
     const groupedData = {};
-  
+
     // Group data by division
     fetchedData.forEach((item) => {
-      if (!groupedData[item.division]) {
-        groupedData[item.division] = [];
-      }
-      groupedData[item.division].push(item);
+        if (!groupedData[item.division]) {
+            groupedData[item.division] = [];
+        }
+        groupedData[item.division].push(item);
     });
 
-  return (
-    <>
-        <p>News Management</p>
-        <div className="DataDisplay">
-            {Object.keys(groupedData).map((division) => (
-                <div key={division}>
-                    <h4>{`${division} (division)`}</h4>
-                    {groupedData[division].map((item) => (
-                        <div key={item._id}>
-                            <p>{`ID: ${item._id}`}</p>
-                            <p>{`Username: ${item.username}`}</p>
-                            <p>{`Password: ${item.password}`}</p>
-                            <hr />
+    const capitalizeFirstLetter = (string) => {
+        // Remove underscores and capitalize each word
+        const stringWithoutUnderscores = string.replace(/_/g, ' ');
+        return stringWithoutUnderscores.replace(/\b\w/g, (match) => match.toUpperCase());
+    };
+
+    return (
+        <>
+            <p>News Management</p>
+            <div className="DataDisplay">
+                {Object.keys(groupedData)
+                    .sort() // Sort keys alphabetically
+                    .map((division) => (
+                        <div key={division}>
+                            <h4>{capitalizeFirstLetter(division)}</h4>
+                            {groupedData[division].map((item) => (
+                                <div key={item._id}>
+                                    <p>{`ID: ${item._id}`}</p>
+                                    <p>{`Username: ${item.username}`}</p>
+                                    <p>{`Password: ${item.password}`}</p>
+                                    <hr />
+                                </div>
+                            ))}
                         </div>
                     ))}
-                </div>
-            ))}
-        </div>
+            </div>
 
-        <Link to="/">Home</Link>
-        <br />
+            <Link to="/">Home</Link>
+            <br />
 
-        <Link to="/AuthPanel">AuthPanel</Link>
-        <br />
-    </>
-  );
+            <Link to="/AuthPanel">AuthPanel</Link>
+            <br />
+        </>
+    );
 }
 
 export default DataDisplay;
