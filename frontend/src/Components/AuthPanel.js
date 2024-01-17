3c4a40fe68957962179b3c938304827da285a036
Jakkalsxl <marco@thereef.co.za>
2024-01-17 18:20:42 +0200

    Changed AuthPanel from class component to funtional component

--- frontend/src/Components/AuthPanel.js
+++ frontend/src/Components/AuthPanel.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import '../App.css';
 
-class AuthPanel extends Component {
-    // Constructor to initialize state
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            password1: '',
-            password2: '',
-            department: '',
-            division: '',
-            isLogin: true, // Flag to determine whether it's a login or registration form
-        };
-    }
+function AuthPanel(props) {
+    const [state, setState] = useState({
+        username: '',
+        password: '',
+        password1: '',
+        password2: '',
+        department: '',
+        division: '',
+        isLogin: true, // Flag to determine whether it's a login or registration form
+    });
+
+    const navigate = useNavigate();
 
     // Handle input changes in the form
-    handleInputChange = (event) => {
+    const handleInputChange = (event) => {
         const { name, value } = event.target;
-        this.setState({ [name]: value });
+        setState((prevState) => ({ ...prevState, [name]: value }));
     };
 
     // Handle form submission
-    handleFormSubmit = async (event) => {
+    const handleFormSubmit = async (event) => {
         event.preventDefault();
 
-        const { username, password, password1, password2, department, isLogin, division } = this.state;
+        const { username, password, password1, password2, department, isLogin, division } = state;
 
         if (isLogin) {
             try {
-                // Make an HTTP request to your backend login endpoint
+                // Make an HTTP request to the backend login endpoint
                 const response = await fetch('/login', {
                     method: 'POST',
                     redirect: 'follow',
@@ -42,13 +40,13 @@ class AuthPanel extends Component {
                 });
 
                 if (response.ok) {
-                    // Successfully logged in, handle the response accordingly (e.g., store the token)
+                    // Successfully logged in, store the token
                     const data = await response.json();
                     localStorage.setItem('token', data.token);
-                    this.props.handleSignIn(data); // Call the handleSignIn function from App.js
-                    this.props.history.push('/'); // Redirect to the homepage
+                    props.handleSignIn(data); // Call the handleSignIn function from App.js
+                    navigate('/'); // Redirect to the homepage
                 } else {
-                    // Handle login failure (e.g., show an error message)
+                    // Handle login failure
                     console.error('Login failed:', response.statusText);
                 }
             } catch (error) {
@@ -62,7 +60,7 @@ class AuthPanel extends Component {
             }
 
             try {
-                // Make an HTTP request to your backend register endpoint
+		// Make an HTTP request to your backend register endpoint
                 const response = await fetch('/register', {
                     method: 'POST',
                     headers: {
@@ -72,10 +70,10 @@ class AuthPanel extends Component {
                 });
 
                 if (response.ok) {
-                    // Successfully registered, handle the response accordingly (e.g., redirect)
-                    this.setState({ isLogin: true }); // Set isLogin to true to switch to the login panel
+		    // Successfully registered, handle the response accordingly (e.g., redirect)
+                    setState((prevState) => ({ ...prevState, isLogin: true })); // Set isLogin to true to switch to the login panel
                 } else {
-                    // Handle registration failure (e.g., show an error message)
+                    // Handle registration failure
                     console.error('Registration failed:', response.statusText);
                 }
             } catch (error) {
@@ -85,92 +83,94 @@ class AuthPanel extends Component {
     };
 
     // Toggle between login and registration forms
-    toggleForm = () => {
+    const toggleForm = () => {
         // Toggle between login and registration forms
-        this.setState((prevState) => ({ isLogin: !prevState.isLogin }));
+        setState((prevState) => ({ ...prevState, isLogin: !prevState.isLogin }));
     };
 
-    render() {
-        const { isLogin } = this.state;
-
-        return (
-            <>
-                <form onSubmit={this.handleFormSubmit} className="auth-form">
-                    <input
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleInputChange}
-                        className="form-input"
-                        placeholder='Username'
-                    />
-                    <br />
-                    <input
-                        type="password"
-                        name={isLogin ? 'password' : 'password1'}
-                        value={isLogin ? this.state.password : this.state.password1}
-                        onChange={this.handleInputChange}
-                        className="form-input"
-                        placeholder='Password'
-                    />
-                    <br />
-                    {!isLogin && (
-                        <>
-                            <input
-                                type="password"
-                                name="password2"
-                                value={this.state.password2}
-                                onChange={this.handleInputChange}
-                                className="form-input"
-                                placeholder='Confirm Password'
-                            />
-                            <br />
-                            <select
-                                id="department"
-                                name="department"
-                                value={this.state.department}
-                                onChange={this.handleInputChange}
-                                className="form-select"
-                            >
-                                <option value="" disabled selected hidden>Select your department</option>
-                                <option value="news_management">News Management</option>
-                                <option value="software_reviews">Software Reviews</option>
-                                <option value="hardware_reviews">Hardware Reviews</option>
-                                <option value="opinion_publishing">Opinion Publishing</option>
-                            </select>
-                            <br />
-                            <select
-                                id="division"
-                                name="division"
-                                value={this.state.division}
-                                onChange={this.handleInputChange}
-                                className="form-select"
-                            >
-                                <option value="" disabled selected hidden>Select your division</option>
-                                <option value="writing">Writing</option>
-                                <option value="editorial">Editorial</option>
-                                <option value="it">IT</option>
-                                <option value="advertising">Advertising</option>
-                                <option value="social_media">Social Media</option>
-                            </select>
-                            <br />
-                        </>
-                    )}
-                    {/* Toggle button to switch between login and registration forms */}
-                    <button type="button" onClick={this.toggleForm} className="toggle-button">{isLogin ? 'Register' : 'Login'}</button>
-                    <br />
-                    {/* Submit button */}
-                    <button type="submit" className="submit-button">{isLogin ? 'Login' : 'Register'}</button>
-                </form>
+    const { isLogin } = state;
+    
+    return (
+        <>
+            <form onSubmit={handleFormSubmit} className="auth-form">
+                <input
+                    type="text"
+                    name="username"
+                    value={state.username}
+                    onChange={handleInputChange}
+                    className="form-input"
+                    placeholder='Username'
+                />
                 <br />
-
-                {/* Display whether it's a login or registration page */}
-                <p>{isLogin ? 'Login' : 'Registration'} page</p>
-                <Link to="/">Home</Link>
+                <input
+                    type="password"
+                    name={isLogin ? 'password' : 'password1'}
+                    value={isLogin ? state.password : state.password1}
+                    onChange={handleInputChange}
+                    className="form-input"
+                    placeholder='Password'
+                />
                 <br />
-            </>
-        );
-    }
+                {!isLogin && (
+                    <>
+                        <input
+                            type="password"
+                            name="password2"
+                            value={state.password2}
+                            onChange={handleInputChange}
+                            className="form-input"
+                            placeholder='Confirm Password'
+                        />
+                        <br />
+                        <select
+                            id="department"
+                            name="department"
+                            value={state.department}
+                            onChange={handleInputChange}
+                            className="form-select"
+                        >
+                            <option value="" disabled selected hidden>Select your department</option>
+                            <option value="news_management">News Management</option>
+                            <option value="software_reviews">Software Reviews</option>
+                            <option value="hardware_reviews">Hardware Reviews</option>
+                            <option value="opinion_publishing">Opinion Publishing</option>
+                        </select>
+                        <br />
+                        <select
+                            id="division"
+                            name="division"
+                            value={state.division}
+                            onChange={handleInputChange}
+                            className="form-select"
+                        >
+                            <option value="" disabled selected hidden>Select your division</option>
+                            <option value="writing">Writing</option>
+                            <option value="editorial">Editorial</option>
+                            <option value="it">IT</option>
+                            <option value="advertising">Advertising</option>
+                            <option value="social_media">Social Media</option>
+                        </select>
+                        <br />
+                    </>
+                )}
+                {/* Toggle button to switch between login and registration forms */}
+                <button type="button" onClick={toggleForm} className="toggle-button">
+                    {isLogin ? 'Register' : 'Login'}
+                </button>
+                <br />
+                {/* Submit button */}
+                <button type="submit" className="submit-button">
+                    {isLogin ? 'Login' : 'Register'}
+                </button>
+            </form>
+            <br />
+
+            {/* Display whether it's a login or registration page */}	
+            <p>{isLogin ? 'Login' : 'Registration'} page</p>
+            <Link to="/">Home</Link>
+            <br />
+        </>
+    );
 }
 
 export default AuthPanel;
